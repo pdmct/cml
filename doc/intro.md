3261ee3a080508d92decfe17456afd7ac7881bd9
adebesin <gregory.adebesin@gmail.com>
2016-04-02 17:12:30 +0100

    Added proj

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cml
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cml/core_test.clj
@@ -0,0 +1,7 @@
+(ns cml.core-test
+  (:require [clojure.test :refer :all]
+            [cml.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
