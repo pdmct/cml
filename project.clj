1e209d1e9498c1c0dfe882d348ad087b96924185
Geem7n <gadebesin@gmail.com>
2016-04-06 21:03:33 +0100

    added deps

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.datumbox/datumbox-framework-lib "0.7.0"]])
+                 [org.apache.commons/commons-math3 "3.6.1"]])
--- src/cml/core/file.clj
+++ src/cml/core/file.clj
@@ -1,17 +1,1 @@
-(ns cml.core.file
-  (:require [clojure.java.io :as io])
-  (:import (java.io BufferedReader InputStreamReader FileInputStream)
-           (java.util.zip GZIPInputStream)))
-
-
-(defmulti read-file (fn [x _] (:file x)))
-
-
-(defmethod read-file :csv
-  [_ {:keys [path]}]
-  (InputStreamReader. (FileInputStream. (io/file path)) "UTF-8"))
-
-
-(defmethod read-file :gzip
-  [_ {:keys [path]}]
-  (BufferedReader. (InputStreamReader. (GZIPInputStream. (FileInputStream. (io/file path))))))
+(ns cml.core.file)
