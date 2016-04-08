9a353c4935aee7b8bbd274b4327fda91ef3147d4
adebesin <gregory.adebesin@gmail.com>
2016-04-08 23:18:32 +0100

    added significance measure

--- src/cml/core/correlation.clj
+++ src/cml/core/correlation.clj
@@ -1,18 +1,19 @@
-(ns cml.core.correlation)
+(ns cml.core.correlation
+  (:require [cml.core.stats.utils :refer :all]))
 
 (def sample {:x [490 500 530 550 580 590 600 600 650 700]
              :y [560 500 510 600 600 620 550 630 650 750 ]})
 
+(defn- deviation-score [mean sample]
+  (map (fn [x]  (- (mean sample) x)) sample))
 
-(defn- mean [sample] (quot (reduce + sample) (count sample)))
-
-(defn- deviation-score [mean sample] (map (fn [x]  (- (mean sample) x)) sample))
-
-(defn- deviation-score-squared [sample] (map (fn [x] (* x x)) (deviation-score mean sample)))
-
-(defn- deviation-score-sum [sample] (reduce + (deviation-score-squared sample)))
 
 (defn pearson-correlation [{:keys [x y]}]
-  (/ (reduce + (map * (deviation-score mean x) (deviation-score mean y))) (Math/sqrt (* (deviation-score-sum x) (deviation-score-sum y)))))
+  (/ (reduce + (map * (deviation-score mean x) (deviation-score mean y)))
+     (Math/sqrt (* (reduce + (squared (deviation-score mean x)))
+                   (reduce + (squared (deviation-score mean y)))))))
 
 
+(defn significance [correlation sample-size]
+  (/ (* correlation (Math/sqrt sample-size))
+     (Math/sqrt (- 1 (* correlation correlation)))))
