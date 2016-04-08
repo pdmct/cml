b85f60f09c3e9be85b72bbdba20ab119382bc4c4
adebesin <gregory.adebesin@gmail.com>
2016-04-08 23:33:14 +0100

    var change

--- src/cml/core/correlation.clj
+++ src/cml/core/correlation.clj
@@ -1,19 +1,23 @@
 (ns cml.core.correlation
   (:require [cml.core.stats.utils :refer :all]))
 
-(def sample {:x [490 500 530 550 580 590 600 600 650 700]
-             :y [560 500 510 600 600 620 550 630 650 750 ]})
-
-(defn- deviation-score [mean sample]
+(defn deviation-score [mean sample]
   (map (fn [x]  (- (mean sample) x)) sample))
 
 
-(defn pearson-correlation [{:keys [x y]}]
-  (/ (reduce + (map * (deviation-score mean x) (deviation-score mean y)))
-     (Math/sqrt (* (reduce + (squared (deviation-score mean x)))
-                   (reduce + (squared (deviation-score mean y)))))))
+(defn pearson-correlation [{:keys [x-axis y-axis]}]
+  (/ (reduce + (map * x-axis y-axis))
+     (Math/sqrt (* (reduce + (squared x-axis))
+                   (reduce + (squared y-axis))))))
 
 
 (defn significance [correlation sample-size]
   (/ (* correlation (Math/sqrt sample-size))
      (Math/sqrt (- 1 (* correlation correlation)))))
+
+
+(def sample {:x-axis (deviation-score mean [490 500 530 550 580 590 600 600 650 700])
+             :y-axis (deviation-score mean [560 500 510 600 600 620 550 630 650 750])})
+
+
+
