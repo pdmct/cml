a5d117e3ae462437c3488a4b4bf182aa0e1d0721
Geem7n <gadebesin@gmail.com>
2016-04-13 20:14:37 +0100

    permutations

--- src/cml/core/utils/stats.clj
+++ src/cml/core/utils/stats.clj
@@ -20,3 +20,14 @@
     (Math/sqrt (sample-mean (map (fn [x] (* (- m x) (- m x))) sample)))))
 
 
+(defn permutations
+  [x xs]
+  (try
+    (letfn [(factorial [x]
+              (loop [cnt (if (coll? x) (count x) x) acc 1]
+                (if (zero? cnt) acc
+                                (recur (dec cnt) (*' cnt acc)))))]
+      (quot (factorial x) (factorial (- x xs))))
+    (catch Exception e nil)))
+
+
