0983eae70ec0f6ee1c0d195ef4d43d8c77507ec5
Geem7n <gadebesin@gmail.com>
2016-04-15 23:30:06 +0100

    variance

--- src/cml/core/hypothesis/test.clj
+++ src/cml/core/hypothesis/test.clj
@@ -2,21 +2,27 @@
   (:require [cml.core.utils.stats :as s]))
 
 
-
 (defn significance [correlation sample-size]
   (/ (* correlation (Math/sqrt sample-size))
      (Math/sqrt (- 1 (* correlation correlation)))))
 
 
-(defn one-sample-t-test [hypothetical-mean sample]
-  (/ (- (s/mean sample) (s/mean hypothetical-mean))
-     (/ (s/standard-deviation {:type :sample} sample) (Math/sqrt (count sample)))))
+(defn t-test [sample hm]
+  {:sample-mean (s/mean sample)
+   :hypothetical-mean (s/mean hm)
+   :sample-variance (s/variance {:type :sample} sample)
+   :sample-size (count sample)})
 
+
+(defn one-sample-t-test [sample mean]
+  ((comp (fn [x] (assoc x :t
+                          (/ (- (:sample-mean x) (:hypothetical-mean x))
+                             (/ (:sample-variance x) (Math/sqrt (:sample-size x)))))))
+    (t-test sample mean)))
 
 
 ;(defn one-sample-t-test :one-tail)
 ;(defn two-sample-t-test :one-tail)
-
 ;(defn one-sample-t-test :two-tail)
 ;(defn two-sample-t-test :two-tail)
 
