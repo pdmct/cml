9193ad0f68ec3e99a77df5aac41bcff6ad3c26d8
Geem7n <gadebesin@gmail.com>
2016-05-10 21:18:31 +0100

    multi methods for stats functions

--- src/cml/core/utils/stats.clj
+++ src/cml/core/utils/stats.clj
@@ -1,6 +1,6 @@
 (ns cml.core.utils.stats)
 
-;TODO change functions to multi methids where appropriate
+;TODO test the variance functions
 
 (defn mean [x] (double (/ (reduce + x) (count x))))
 
@@ -17,41 +17,40 @@
     (Math/sqrt (mean (map (fn [x] (* (- m x) (- m x))) (:val type))))))
 
 
-(defmethod standard-deviation :sample [x sample]
-  (let [m (mean sample)]
-    (Math/sqrt (mean-1 (map (fn [x] (* (- m x) (- m x))) sample)))))
+(defmethod standard-deviation :sample [type]
+  (let [m (mean (:val type))]
+    (Math/sqrt (mean-1 (map (fn [x] (* (- m x) (- m x))) (:val type))))))
 
 
-(defmulti variance (fn [x _] (:variance x)))
+(defmulti variance (fn [type] (:type type)))
 
-(defmethod variance :population [x population]
-  (let [m (mean population)]
-    (/ (reduce + (map (fn [x] (* (- x m) (- x m))) population)) (count population))))
-
-
-(defmethod variance :sample [x sample]
-  (let [m (mean sample)]
-    (/ (reduce + (map (fn [x] (* (- x m) (- x m))) sample))
-       (dec (count sample)))))
+(defmethod variance :population [type]
+  (let [m (mean (:val type))]
+    (/ (reduce + (map (fn [x] (* (- x m) (- x m))) (:val type))) (count (:val type)))))
 
 
-(defmethod variance :pooled [x sample]
-  (let [c (- (count sample) 1)]
-    (/ (* c (variance {:variance :sample} sample)) c)))
+(defmethod variance :sample [type]
+  (let [m (mean (:val type))]
+    (/ (reduce + (map (fn [x] (* (- x m) (- x m))) (:val type)))
+       (dec (count (:val type))))))
+
+
+(defmethod variance :pooled [type]
+  (let [c (- (count (:val type)) 1)]
+    (/ (* c (variance {:val (:val type) :type :sample})) c)))
 
 
 (defn permutations
   [x xs]
-  (try
-    (letfn [(factorial [x]
-              (loop [cnt (if (coll? x) (count x) x) acc 1]
-                (if (zero? cnt) acc
-                                (recur (dec cnt) (*' cnt acc)))))]
-      (quot (factorial x) (factorial (- x xs))))
-    (catch Exception e nil)))
+  (letfn [(factorial [x]
+            (loop [cnt (if (coll? x) (count x) x) acc 1]
+              (if (zero? cnt) acc
+                              (recur (dec cnt) (*' cnt acc)))))]
+    (quot (factorial x) (factorial (- x xs)))))
 
 
 (defn significance [correlation sample-size]
   (/ (* correlation (Math/sqrt sample-size))
      (Math/sqrt (- 1 (* correlation correlation)))))
+
 
