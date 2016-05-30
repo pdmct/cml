9f551dca82edba718cdc97a98ef22b3e4b473fb3
Geem7n <gadebesin@gmail.com>
2016-05-30 22:44:42 +0100

    tidy

--- src/cml/core/utils/stats.clj
+++ src/cml/core/utils/stats.clj
@@ -14,43 +14,71 @@
 
 (defmethod standard-deviation :population [type]
   (let [m (mean (:data type))]
-    (Math/sqrt (mean (map (fn [x] (* (- m x) (- m x))) (:data type))))))
+    (Math/sqrt (mean (map (fn [x] (* (- m
+                                        x)
+                                     (- m
+                                        x)))
+                          (:data type))))))
 
 
 (defmethod standard-deviation :sample [type]
   (let [m (mean (:data type))]
-    (Math/sqrt (mean-1 (map (fn [x] (* (- m x) (- m x))) (:data type))))))
+    (Math/sqrt (mean-1 (map (fn [x] (* (- m
+                                          x)
+                                       (- m
+                                          x)))
+                            (:data type))))))
 
 
 (defmulti variance (fn [type] (:type type)))
 
 (defmethod variance :population [type]
   (let [m (mean (:data type))]
-    (/ (reduce + (map (fn [x] (* (- x m) (- x m))) (:data type))) (count (:data type)))))
+    (/ (reduce +
+               (map (fn [x] (* (- x
+                                  m)
+                               (- x
+                                  m)))
+                    (:data type)))
+       (count (:data type)))))
 
 
 (defmethod variance :sample [type]
   (let [m (mean (:data type))]
-    (/ (reduce + (map (fn [x] (* (- x m) (- x m))) (:data type)))
+    (/ (reduce +
+               (map (fn [x] (* (- x
+                                  m)
+                               (- x
+                                  m)))
+                    (:data type)))
        (dec (count (:data type))))))
 
 
 (defmethod variance :pooled [type]
   (let [c (- (count (:data type)) 1)]
-    (/ (* c (variance {:data (:data type) :type :sample})) c)))
+    (/ (* c
+          (variance {:data (:data type) :type :sample}))
+       c)))
 
 
 (defn permutations
   [x xs]
   (letfn [(factorial [x]
             (loop [cnt (if (coll? x) (count x) x) acc 1]
-              (if (zero? cnt) acc
-                              (recur (dec cnt) (*' cnt acc)))))]
-    (quot (factorial x) (factorial (- x xs)))))
+              (if (zero? cnt)
+                acc (recur (dec cnt)
+                           (*' cnt
+                               acc)))))]
+    (quot (factorial x)
+          (factorial (- x
+                        xs)))))
 
 
 (defn significance [correlation sample-size]
-  (/ (* correlation (Math/sqrt sample-size))
-     (Math/sqrt (- 1 (* correlation correlation)))))
+  (/ (* correlation
+        (Math/sqrt sample-size))
+     (Math/sqrt (- 1
+                   (* correlation
+                      correlation)))))
 
 
