657c4775b36d953c62d6a5e9b24617421cd06781
Geem7n <gadebesin@gmail.com>
2016-06-08 20:59:51 +0100

    changed algo

--- src/cml/core/dataset.clj
+++ src/cml/core/dataset.clj
@@ -4,11 +4,6 @@
 
 ;TODO turn functions into transducers
 
-(defn- split-str [^CharSequence s ^Pattern re]
-  (lazy-seq (.split re
-                    s)))
-
-
 (defn- file-lines [file]
      (letfn [(helper [rdr]
                   (lazy-seq (if-let [line (.readLine rdr)]
@@ -19,47 +14,47 @@
        (helper (clojure.java.io/reader file))))
 
 
+(defn- zipmap-types [keys vals]
+  (loop [map {}
+         ks (seq keys)
+         vs (seq vals)]
+    (if (and ks
+             vs)
+      (recur (assoc map
+                (first ks)
+               (cond (= (second ks)
+                        :string)
+                     (first vs)
+                     (=  (second ks)
+                        :integer)
+                     (Integer/parseInt (first vs))
+                     (= (second ks)
+                        :long)
+                     (Long/parseLong (first vs))
+                     (= (second ks)
+                        :double)
+                     (Double/parseDouble (first vs))
+                     (= (second ks)
+                        :character)
+                     (.charAt (first vs) 0)
+                     :else (first vs)))
+             (drop 2 ks)
+             (next vs)) map)))
+
+
 (defn data-frame [^String file-path
                   ^Pattern re
                   ^PersistentVector column-names]
      (map (fn [x] (zipmap column-names
-                          (split-str x
-                                     re)))
+                          (clojure.string/split x re)))
           (file-lines file-path)))
 
 
 (defn data-frame-types [^String file-path
                         ^Pattern re
                         ^PersistentVector column-names]
-  (letfn [(zipmap-types [keys vals]
-            (loop [map {}
-                   ks (seq keys)
-                   vs (seq vals)]
-              (if (and ks
-                       vs)
-                (recur (assoc map
-                         ((first ks) 0)
-                         (cond (= (second (first ks))
-                                  :string)
-                               (first vs)
-                               (= (second (first ks))
-                                  :integer)
-                               (Integer/parseInt (first vs))
-                               (= (second (first ks))
-                                  :long)
-                               (Long/parseLong (first vs))
-                               (= (second (first ks))
-                                  :double)
-                               (Double/parseDouble (first vs))
-                               (= (second (first ks))
-                                  :character)
-                               (.charAt (first vs) 0)
-                               :else (first vs)))
-                       (next ks)
-                       (next vs)) map)))]
-    (map (fn [x] (zipmap-types column-names
-                               (split-str x
-                                          re)))
-         (file-lines file-path))))
+  (map (fn [x] (zipmap-types column-names
+                             (clojure.string/split x re)))
+       (file-lines file-path)))
 
 
