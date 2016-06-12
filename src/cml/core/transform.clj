0ab7a178ac0696f01488f43cc88544d8322d1f98
adebesin <gregory.adebesin@gmail.com>
2016-06-12 23:29:32 +0100

    xform-by-key

--- src/cml/core/transform.clj
+++ src/cml/core/transform.clj
@@ -5,5 +5,10 @@
   `(reduce-kv (fn [m1# k# v#]
                 (assoc m1# k# ((comp ~@fns) v#))) {} ~m))
 
+(defmacro xform-by-key-test [m & kfns]
+  (cons (cons 'comp
+              (for [i (range 0 (count kfns))]
+                `(~'fn [x#] (~'update x# :department clojure.string/upper-case))))
+        `(~m)))
 
-(defn xform-by-key [])
+
