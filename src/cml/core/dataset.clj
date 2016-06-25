4b77aff9a732a63ae86c43f31303b5406bfc138a
adebesin <gregory.adebesin@gmail.com>
2016-06-25 21:35:53 +0100

    transduce

--- src/cml/core/dataset.clj
+++ src/cml/core/dataset.clj
@@ -1,9 +1,9 @@
 (ns cml.core.dataset
   (:require [cml.core.utils :refer [zip]]
             [cml.core.transform :refer [line-split]]
-            [cml.core.extract :refer [file-lines]])
-  (:import (org.apache.poi.ss.usermodel Cell Row Sheet Workbook WorkbookFactory)
-           (java.io FileInputStream)))
+            [cml.core.extract :refer [file-lines]]
+            [cml.core.utils.file.excel.cell :only  [read-line-values write-line-values]]
+            [cml.core.utils.file.excel.workbook :only [get-workbook-sheet make-workbook-map write-workbook create-workbook-object create-sheet get-all-sheets]]))
 
 ;TODO Design how data frames will be composable with statistical functions
 ;TODO Implementparallel data frame
@@ -13,13 +13,11 @@
 
 (defn xform-csv
   ([column-names delim]
-   (comp
-     (map #(line-split % delim))
-     (map #(zip column-names %))))
+   (comp (map #(line-split % delim))
+         (map #(zip column-names %))))
   ([column-names delim xform]
-   (comp
-     (map #(line-split % delim))
-     (map #(zip column-names % xform)))))
+   (comp (map #(line-split % delim))
+         (map #(zip column-names % xform)))))
 
 
 (defmethod data-frame :csv [type]
