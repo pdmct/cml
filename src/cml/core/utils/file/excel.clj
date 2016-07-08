295c243371cc4d010ad8a025ed46179df5b6a5e9
adebesin <gregory.adebesin@gmail.com>
2016-07-09 00:17:52 +0100

    excel

--- src/cml/core/utils/file/excel.clj
+++ src/cml/core/utils/file/excel.clj
@@ -3,43 +3,34 @@
             [clojure.zip :as zip]
             [clojure.data.xml :as x])
   (:import (org.apache.poi.ss.usermodel Cell Row Sheet Workbook WorkbookFactory)
-           (org.apache.poi.ss.util NumberToTextConverter)))
+           (org.apache.poi.ss.util NumberToTextConverter CellReference)))
 
 (defn load-workbook
   "Load a workbook from a string path."
   [path]
    (WorkbookFactory/create (clojure.java.io/input-stream path)))
 
+
 (defn read-sheet
   ([workbook] (read-sheet workbook "Sheet1"))
   ([workbook sheet-name]
    (let [sheet   (.getSheet workbook sheet-name)
-         rows    (iterator-seq (.iterator sheet))]
-     rows)))
-
-(defn read-sheet-test
-  ([workbook] (read-sheet workbook "Sheet1"))
-  ([workbook sheet-name]
-   (let [sheet   (.getSheet workbook sheet-name)]
-     (while (println  (seq (.rowIterator sheet)))))))
-
-;TODO wrap poi api in protocols
-
+         rows    (iterator-seq (.rowIterator sheet))]
+     (for [y (range 0 (.getLastRowNum sheet))]
+       (map (fn [x]
+              (cond (nil? (.getCell x y Row/RETURN_BLANK_AS_NULL))
+                    nil
+                    (= Cell/CELL_TYPE_NUMERIC (.getCellType (.getCell x y)))
+                    (NumberToTextConverter/toText (.getNumericCellValue (.getCell x y)))
+                    :else (.getStringCellValue (.getCell x y))))
+            (rest rows))))))
 
 
-(defn read-sheet-test
+(defn read-sheet
   ([workbook] (read-sheet workbook "Sheet1"))
   ([workbook sheet-name]
    (let [sheet   (.getSheet workbook sheet-name)
-         rows    (iterator-seq (.rowIterator sheet))]
-     (map (fn [x]
-            (if (nil? (.getCell x 1 Row/RETURN_BLANK_AS_NULL))
-              nil
-              (if (= Cell/CELL_TYPE_NUMERIC (.getCellType (.getCell x 1)))
-                (NumberToTextConverter/toText (.getNumericCellValue (.getCell x 1)))
-                (.getStringCellValue (.getCell x 1)))))
-          rows))))
+         rows    (iterator-seq (.rowIterator sheet))
+         cells  (map (fn [x] (.toString(.cellIterator x))) rows)]
+     cells)))
 
-
-(rest (map (fn [y] (mapcat :content (mapcat :content (:content (x/parse-str (str y))))))
-           (read-sheet (load-workbook "/Users/gra11/Downloads/bank.xlsx") "D6")))
