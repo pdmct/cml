e9f382ac92d41b1f8e1b4324e2d6bc5e02141c12
Geem7n <gadebesin@gmail.com>
2016-07-21 22:09:51 +0100

    protocol types

---
+++ src/cml/core/inference/estimate/confidence-interval.clj
@@ -0,0 +1,57 @@
+(ns cml.core.inference.estimate.confidence-interval)
+
+;TODO Have functions comply with dataframes
+
+;TODO have in out map
+
+(defprotocol Estimate
+  (one-sample-confidence-interval-test [os] "One sample confidence interval estimation")
+  (two-sample-confidence-interval-test [ts] "Two sample confidence interval estimation"))
+
+(defn one-sample-confidence-interval [mean standard-deviation size critical-value]
+  {:mean mean
+   :standard-deviation standard-deviation
+   :size size
+   :critical-value critical-value
+   :Estimate :OneSample})
+
+
+(defn two-sample-confidence-interval [mean variance size critical-value]
+  {:mean mean
+   :variance variance
+   :size size
+   :critical-value critical-value
+   :Estimate :TwoSample})
+
+
+(defrecord ConfidenceInterval [confidence-interval]
+  Estimate
+
+  (one-sample-confidence-interval-test [type]
+    (assoc type
+      :upper (+ (:mean confidence-interval)
+                (* (:critical-value confidence-interval)
+                   (/ (:standard-deviation confidence-interval)
+                      (Math/sqrt (:size confidence-interval)))))
+      :lower (- (:mean confidence-interval)
+                (* (:critical-value confidence-interval)
+                   (/ (:standard-deviation confidence-interval)
+                      (Math/sqrt (:size confidence-interval)))))))
+  (two-sample-confidence-interval-test [type]
+    (assoc type
+      :upper (+ (- ((:mean confidence-interval) 0)
+                   ((:mean confidence-interval) 1))
+                (* (:critical-value confidence-interval)
+                   (Math/sqrt (+ (/ ((:variance confidence-interval) 0)
+                                    ((:size confidence-interval) 0))
+                                 (/ ((:variance confidence-interval) 1)
+                                    ((:size confidence-interval) 1))))))
+      :lower (- (- ((:mean confidence-interval) 0)
+                   ((:mean confidence-interval) 1))
+                (* (:critical-value confidence-interval)
+                   (Math/sqrt (+ (/ ((:variance confidence-interval) 0)
+                                    ((:size confidence-interval) 0))
+                                 (/ ((:variance confidence-interval) 1)
+                                    ((:size confidence-interval) 1)))))))))
+
+
---
+++ src/cml/core/inference/test/t-test.clj
@@ -0,0 +1,135 @@
+(ns cml.core.inference.test.t-test
+  (:require [cml.core.inference.tables :refer [t-table]]))
+(use 'clojure.core.matrix)
+
+;TODO Have functions comply with dataframes
+;TODO have in out map
+
+(defprotocol Test
+  (one-sample [one] "One tail t-test")
+  (equal-variance [two] "Two tail t-test")
+  (welch [welch] "Welsch's t-test")
+  (repeated-measure [rm] "Repeated measure t-test")
+  (one-tail [ot] "One tail significance test")
+  (two-tail [tt] "Two tail significance test"))
+
+(defn one-sample-t-test [mean standard-deviation hypothetical-mean size]
+  {:mean mean
+   :standard-deviation standard-deviation
+   :hypothetical-mean hypothetical-mean
+   :size size
+   :Test :OneSample})
+
+(defn equal-variance-t-test [sample-mean population-mean pooled-variance size]
+  {:sample-mean sample-mean
+   :population-mean population-mean
+   :pooled-variance pooled-variance
+   :size size
+   :Test :EqualVariance})
+
+(defn welch-t-test [mean sample-variance size]
+  {:mean mean
+   :sample-variance sample-variance
+   :size size
+   :Test :Welch})
+
+(defn repeated-measure-t-test [difference-mean population-mean standard-deviation size]
+  {:difference-mean difference-mean
+   :population-mean population-mean
+   :standard-deviation standard-deviation
+   :size size
+   :Test :RepeatedMeasure})
+
+(defrecord TTest [ttest]
+  Test
+  (one-sample [type]
+    (assoc type
+      :t-statistic (/ (- (:mean ttest)
+                         (:hypothetical-mean ttest))
+                      (/ (:standard-deviation ttest)
+                         (Math/sqrt (:size ttest))))
+
+      :dof (dec (:size ttest))))
+
+  (equal-variance [type]
+    (assoc type
+      :t-statistic (/ (- (- ((:sample-mean ttest) 0)
+                            ((:sample-mean ttest) 1))
+                         (- ((:population-mean ttest) 0)
+                            ((:population-mean ttest) 1)))
+                      (Math/sqrt (* (/ (+ ((:pooled-variance ttest) 0)
+                                          ((:pooled-variance ttest) 1))
+                                       2)
+                                    (+ (/ 1
+                                          ((:size ttest) 0))
+                                       (/ 1
+                                          ((:size ttest) 1))))))
+      :dof (- (+ ((:size ttest) 0)
+                 ((:size ttest) 1)) 2)))
+
+  (welch [type]
+    (assoc type
+      :t-statistic (/ (- ((:mean ttest) 0)
+                         ((:mean ttest) 1))
+                      (Math/sqrt (+ (/ ((:sample-variance ttest) 0)
+                                       ((:size ttest) 0))
+                                    (/ ((:sample-variance ttest) 1)
+                                       ((:size ttest) 1)))))
+      :dof (/ (* (+ (/ ((:sample-variance ttest) 0)
+                       ((:size ttest) 0))
+                    (/ ((:sample-variance ttest) 1)
+                       ((:size ttest) 1)))
+                 (+ (/ ((:sample-variance ttest) 0)
+                       ((:size ttest) 0))
+                    (/ ((:sample-variance ttest) 1)
+                       ((:size ttest) 1))))
+              (+ (/ (* (/ ((:sample-variance ttest) 0)
+                          ((:size ttest) 0))
+                       (/ ((:sample-variance ttest) 0)
+                          ((:size ttest) 0)))
+                    (- ((:size ttest) 0)
+                       1))
+                 (/ (* (/ ((:sample-variance ttest) 1)
+                          ((:size ttest) 1))
+                       (/ ((:sample-variance ttest) 1)
+                          ((:size ttest) 1)))
+                    (- ((:size ttest) 1)
+                       1))))))
+
+  (repeated-measure [type]
+    (assoc type
+      :t-statistic (/ (- (:difference-mean ttest)
+                         (- ((:population-mean ttest) 0)
+                            ((:population-mean ttest) 1)))
+                      (/ (:standard-deviation ttest)
+                         (Math/sqrt (:size ttest))))
+      :dof (- (:size ttest) 1))))
+
+
+;TODO Move significance test out so comply with protocol naming convention / file structure
+(defn one-tail-significance-test [dof alpha]
+  {:dof dof
+   :alpha alpha
+   :Test :OneTail})
+
+
+(defn two-tail-significance-test [dof alpha]
+  {:dof dof
+   :alpha alpha
+   :Test :TwoTail})
+
+(defrecord SignificanceTest [significance-test]
+  Test
+  (one-tail [type]
+    (assoc type
+      :critical-value (mget t-table (dec (:dof significance-test))
+                            ({0.05 0 0.025 1 0.01 2 0.005 3 0.0025 4 0.001 5 0.0005 6}
+                                             (:alpha significance-test)))))
+
+  (two-tail [type]
+    (assoc type
+      :critical-value (mget t-table (dec (:dof significance-test))
+                            ({0.1 0 0.05 1 0.02 2 0.01 3 0.005 4 0.002 5 0.001 6}
+                              (:alpha significance-test))))))
+
+
