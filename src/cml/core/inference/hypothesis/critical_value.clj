31790ef52d07a34b1e95680b1dc7094c2f545f0b
Geem7n <gadebesin@gmail.com>
2016-08-07 22:20:40 +0100

    confidencr interval

--- src/cml/core/inference/hypothesis/critical_value.clj
+++ src/cml/core/inference/hypothesis/critical_value.clj
@@ -13,57 +13,33 @@
   (let [mean ^double (mean data)]
     (t-test (OneSample.
               mean
-              (:standard-deviation (standard-deviation
-                                     (Sample. mean data)))
+              (:standard-deviation (standard-deviation (Sample. mean data)))
               h-mean
               (count data)))))
 
-
-(defn two-sample-ttest [{:keys [^PersistentVector samples ^PersistentVector hp-means]}]
-  (let [[sample-one sample-two] samples
-        [sample-mean-one sample-mean-two] (map mean samples)
-        [sample-size-one sample-size-two] (map count samples)]
-    (t-test (TwoSample.
-              [sample-mean-one sample-mean-two]
-              (map mean (partition 1 hp-means))
-              [(:variance (variance
-                            (Pooled. sample-mean-one sample-one
-                                     (- sample-size-one 1))))
-               (:variance (variance
-                            (Pooled. sample-mean-two sample-two
-                                     (- sample-size-two 1))))]
-              [sample-size-one sample-size-two]))))
+;Make Multi Method this function is equal variance ttest
+(defn two-sample-ttest [{:keys [^PersistentVector sample ^PersistentVector hp-mean]}]
+  (t-test (TwoSample.
+            (map mean sample)
+            (map mean (partition 1 hp-mean))
+            (map #(:variance (variance (Pooled. (mean %) % (- (count %) 1)))) sample)
+            (map count sample))))
 
 
 
-(defn welch-ttest [{:keys [^PersistentVector samples]}]
-  (let [[sample-one sample-two] samples
-        [sample-mean-one sample-mean-two] (map mean samples)
-        [sample-count-one sample-count-two] (map count samples)]
-    (t-test (Welch. [sample-mean-one sample-mean-two]
-                    [(:variance (variance
-                                  (Sample. sample-mean-one sample-one)))
-                     (:variance (variance
-                                  (Sample. sample-mean-two sample-two)))]
-                    [sample-count-one sample-count-two]))))
+(defn welch-ttest [{:keys [^PersistentVector sample]}]
+  (t-test (Welch. (map mean sample)
+                  (map #(:variance (variance (Sample. (mean %) %))) sample)
+                  (map count sample))))
 
 
-(defn rep-measure-ttest [{:keys [^PersistentVector populations ^PersistentVector hp-means]}]
-  (let [[population-one population-two] populations
-        population-mean-difference ^double (mean (difference populations))]
-    (t-test
-      (RepeatedMeasure.
-        population-mean-difference
-        (map mean (partition 1 hp-means))
-        (:standard-deviation (standard-deviation
-                               (Sample. population-mean-difference
-                                        (difference populations))))
+(defn rep-measure-ttest [{:keys [^PersistentVector population ^PersistentVector hp-mean]}]
+  (let [[population-one population-two] population
+        population-mean-difference ^double (mean (difference population))]
+    (t-test (RepeatedMeasure.
+              population-mean-difference
+              (map mean (partition 1 hp-mean))
+              (:standard-deviation (standard-deviation (Sample. population-mean-difference (difference population))))
         (/ (+ (count population-one) (count population-two)) 2)))))
-
-
-(defn one-tail-sig-test [{:keys [dof alpha]}] (significance (OneTail. dof alpha)))
-
-
-(defn two-tail-sig-test [{:keys [dof alpha]}] (significance (TwoTail. dof alpha)))
 
 
