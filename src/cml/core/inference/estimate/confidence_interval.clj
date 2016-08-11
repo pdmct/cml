1ebc68aaad5bf772a4dd68f0f1f521d511632ff2
Geem7n <gadebesin@gmail.com>
2016-08-11 22:19:53 +0100

    confidencr interval

--- src/cml/core/inference/estimate/confidence_interval.clj
+++ src/cml/core/inference/estimate/confidence_interval.clj
@@ -3,23 +3,17 @@
             [cml.inference.estimate.confidence-interval :refer [confidence-interval]]
             [cml.statistics.central-tendancy :refer [mean]])
   (:import [cml.statistics.variation Sample]
-           [cml.inference.estimate.confidence_interval OneSample TwoSample]
-           [clojure.lang PersistentVector]))
+           [cml.inference.estimate.confidence_interval OneSample TwoSample]))
 
 
-(defn one-sample-conf-inter
-  [{:keys [^PersistentVector sample critical-value]}]
-  (confidence-interval (OneSample.
-                         (mean sample)
-                         (:standard-deviation (standard-deviation (Sample. (mean sample) sample)))
-                         (count sample) critical-value)))
+(defn one-sample-conf-inter [{:keys [sample critical-value]}]
+  (confidence-interval (OneSample. (mean sample)
+                                   (:standard-deviation (standard-deviation (Sample. (mean sample) sample)))
+                                   (count sample) critical-value)))
 
 
-(defn two-sample-conf-inter
-  [{:keys [^PersistentVector sample critical-value]}]
-  (confidence-interval (TwoSample.
-                         (map mean sample)
-                         (map #(:variance (variance (Sample. (mean %) %))) sample)
-                         (map count sample) critical-value)))
-
+(defn two-sample-conf-inter [{:keys [sample critical-value]}]
+  (confidence-interval (TwoSample. (map mean sample)
+                                   (map #(:variance (variance (Sample. (mean %) %))) sample)
+                                   (map count sample) critical-value)))
 
--- src/cml/core/inference/hypothesis/critical_value.clj
+++ src/cml/core/inference/hypothesis/critical_value.clj
@@ -2,44 +2,36 @@
   (:require [cml.inference.hypothesis.critical-value :refer [t-test significance]]
             [cml.statistics.variation :refer [standard-deviation variance]]
             [cml.statistics.central-tendancy :refer [mean difference]])
-  (:import [cml.inference.hypothesis.critical_value OneSample TwoSample Welch RepeatedMeasure OneTail TwoTail]
-           [cml.statistics.variation Sample Pooled]
-           [clojure.lang PersistentVector]))
-(use 'criterium.core)
+  (:import [cml.inference.hypothesis.critical_value OneSample EqualVariance Welch RepeatedMeasure]
+           [cml.statistics.variation Sample Pooled]))
 
 ;TODO start documenting all functions
 
-(defn one-sample-ttest [{:keys [^PersistentVector data h-mean]}]
-  (let [mean ^double (mean data)]
-    (t-test (OneSample.
-              mean
-              (:standard-deviation (standard-deviation (Sample. mean data)))
-              h-mean
-              (count data)))))
-
-;Make Multi Method this function is equal variance ttest
-(defn two-sample-ttest [{:keys [^PersistentVector sample ^PersistentVector hp-mean]}]
-  (t-test (TwoSample.
-            (map mean sample)
-            (map mean (partition 1 hp-mean))
-            (map #(:variance (variance (Pooled. (mean %) % (- (count %) 1)))) sample)
-            (map count sample))))
+(defn one-sample-ttest [{:keys [sample h-mean]}]
+  (let [mean ^double (mean sample)]
+    (t-test (OneSample. mean (:standard-deviation (standard-deviation (Sample. mean sample)))
+                        h-mean
+                        (count sample)))))
 
 
+(defn equal-var-ttest [{:keys [sample hp-mean]}]
+  (t-test (EqualVariance. (map mean sample)
+                          (map mean (partition 1 hp-mean))
+                          (map #(:variance (variance (Pooled. (mean %) % (- (count %) 1)))) sample)
+                          (map count sample))))
 
-(defn welch-ttest [{:keys [^PersistentVector sample]}]
+
+(defn welch-ttest [{:keys [sample]}]
   (t-test (Welch. (map mean sample)
                   (map #(:variance (variance (Sample. (mean %) %))) sample)
                   (map count sample))))
 
 
-(defn rep-measure-ttest [{:keys [^PersistentVector population ^PersistentVector hp-mean]}]
+(defn rep-measure-ttest [{:keys [population hp-mean]}]
   (let [[population-one population-two] population
         population-mean-difference ^double (mean (difference population))]
-    (t-test (RepeatedMeasure.
-              population-mean-difference
-              (map mean (partition 1 hp-mean))
-              (:standard-deviation (standard-deviation (Sample. population-mean-difference (difference population))))
-        (/ (+ (count population-one) (count population-two)) 2)))))
-
+    (t-test (RepeatedMeasure. population-mean-difference
+                              (map mean (partition 1 hp-mean))
+                              (:standard-deviation (standard-deviation (Sample. population-mean-difference (difference population))))
+                              (/ (+ (count population-one) (count population-two)) 2)))))
 
